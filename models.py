897a9b4373f4700443a79d53c8fa22de22473f1d
9hax <paule.pauling@gmail.com>
2022-11-23 12:21:09 +0100

    add additionla database table columns

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(400), unique=True, nullable=False)
@@ -8,35 +9,50 @@ class User(db.Model):
     email = db.Column(db.String(400), unique=True, nullable=True)
     password = db.Column(db.String(1000), unique=False, nullable=True)
     passwordToken = db.Column(db.Text, unique=False, nullable=True)
-    passwordResetTimer = db.Column(db.Integer, unique=False, nullable=True, default=-1)
-    highPermissionLevel = db.Column(db.Boolean, unique=False, nullable=False, default=False)
+    passwordResetTimer = db.Column(
+        db.Integer, unique=False, nullable=True, default=-1)
+    isRestaurant = db.Column(
+        db.Boolean, unique=False, nullable=True, default=False)
+    highPermissionLevel = db.Column(
+        db.Boolean, unique=False, nullable=False, default=False)
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r, %s, %s>' % self.username, self.email, self.isRestaurant
+
 
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(512), unique=False, nullable=True)
-    is_open = db.Column(db.Boolean, unique=False, nullable = False, default= True)
+    is_open = db.Column(db.Boolean, unique=False, nullable=False, default=True)
     text = db.Column(db.Text, unique=False, nullable=False)
-    media = db.Column(db.Text, unique=False, nullable=True) #This contains base64'ed binary images and videos in a python list.
-    time = db.Column(db.Integer, unique = False) # The time the ticket was created in epoch seconds
+    # This contains base64'ed binary images and videos in a python list.
+    media = db.Column(db.Text, unique=False, nullable=True)
+    # The time the ticket was created in epoch seconds
+    time = db.Column(db.Integer, unique=False)
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    hidden = db.Column(db.Boolean, unique=False, default= False)
+    hidden = db.Column(db.Boolean, unique=False, default=False)
 
-    created_by = db.relationship('User', backref='tickets_created_by', foreign_keys=[created_by_id])
+    created_by = db.relationship(
+        'User', backref='tickets_created_by', foreign_keys=[created_by_id])
+
     def __repr__(self):
-        return '<Ticket %r>' % self.title
+        return '<Ticket %s: %r>' % (self.id, self.title)
+
 
 class TicketReply(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, unique=False, nullable=False)
     media = db.Column(db.Text, unique=False, nullable=True)
-    isNote = db.Column(db.Boolean, unique=False, nullable = True)
-    time = db.Column(db.Integer, unique = False) # The time the ticket reply was created in epoch seconds
+    isNote = db.Column(db.Boolean, unique=False, nullable=True)
+    # The time the ticket reply was created in epoch seconds
+    time = db.Column(db.Integer, unique=False)
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     main_ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
 
-    created_by = db.relationship('User', backref='ticket_reply_by', foreign_keys=[created_by_id])
-    main_ticket = db.relationship('Ticket', backref = 'ticket_reply_main_ticket', foreign_keys=[main_ticket_id])
+    created_by = db.relationship(
+        'User', backref='ticket_reply_by', foreign_keys=[created_by_id])
+    main_ticket = db.relationship(
+        'Ticket', backref='ticket_reply_main_ticket', foreign_keys=[main_ticket_id])
+
     def __repr__(self):
-        return '<TicketReply to %r>' % self.main_ticket
+        return '<TicketReply to %s>' % self.main_ticket
